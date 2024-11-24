ea74116771846c48c0d43a49a49c5dc937fe9389
Taylor <admin@vulehuan.com>
2024-11-24 07:04:53 +0700

    Implement authentication with JWT and authorization with CanCanCan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+
+  private
+
+  def current_ability
+    @current_ability ||= Ability.new(@current_user_role, @current_user_id)
+  end
+
+  def authenticate_request
+    token = request.headers['Authorization']&.split(' ')&.last
+
+    if token
+      begin
+        payload = JWT.decode(token, ENV['JWT_KEY'], true, algorithm: 'HS256')
+        @current_user_role = payload[0]['role']
+        @current_user_id = payload[0]['id']
+      rescue JWT::DecodeError => e
+        render json: { error: 'Invalid or expired token', message: e.message }, status: :unauthorized
+      end
+    else
+      render json: { error: 'Token not provided' }, status: :unauthorized
+    end
+  end
 end
---
+++ app/models/ability.rb
@@ -0,0 +1,14 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(current_user_role, current_user_id)
+    can :create, Order
+
+    if current_user_role == 'admin'
+      can :manage, :all
+    elsif current_user_role == 'user'
+      can :manage, Order, user_id: current_user_id
+      can :read, OrderStatusHistory, order: { user_id: current_user_id }
+    end
+  end
+end
